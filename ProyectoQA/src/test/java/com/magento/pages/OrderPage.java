4c9659b0624660db66ee3d2889a6aadfcb9a8a7d
AngelChavez <angel46_8@hotmail.com>
2024-10-11 18:26:48 -0500

    cambios

--- ProyectoQA/src/test/java/com/magento/pages/OrderPage.java
+++ ProyectoQA/src/test/java/com/magento/pages/OrderPage.java
@@ -2,17 +2,21 @@ package com.magento.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class OrderPage extends BasePage{
 
 	protected WebDriver driver;
+	
+	Actions acciones = new Actions(driver);
 	
 	By signInLink = By.xpath("//div[@class='panel header']//a[contains(text(),'Sign In')]");
 	By email = By.id("email_address");
 	By email1 = By.id("email");
 	By password =By.id("pass") ;
 	By btnIngLogin = By.xpath("//fieldset[@class='fieldset login']//span[contains(text(),'Sign In')]");
-	By dropdown = By.xpath("(//span[contains(text(),'Women')])[1]");
+	//By dropdown = By.xpath("//a[@id='ui-id-5']//span[@class='ui-menu-icon ui-icon ui-icon-carat-1-e']");
+	
 	
 	
 	
@@ -28,8 +32,12 @@ public class OrderPage extends BasePage{
 	       driver.findElement(email1).sendKeys("angel46_318@hotmail.com");
 	       driver.findElement(password).sendKeys("Elpeleador90@");
 	       driver.findElement(btnIngLogin).click();
-	       driver.findElement(dropdown).click();
-            
+	       acciones.moveToElement(driver.findElement(By.xpath("//*[@id=\"ui-id-5\"]/span[2]")));
+	       
+	       
+	       //driver.findElement(dropdown).click();
+	      // driver.findElement(menu).click();
+	      // driver.findElement(menu1).click();
 	}
 	
 	
